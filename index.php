fe14eea572f0348f02ae49303cffd278198b532b
Nick <nickspierenburg@hotmail.com>
2018-04-11 15:20:04 +0200

    Finalized color choices and setup

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ require_once('classes/classes.php');
 $hash = ['Head shape' => 00, 'Ear shape' => 00, 'Mouth shape' => 00, 'Frontpaws shape' => 00, 'Backpaws shape' => 00, 'Tail shape' => 00, 'Head color' => 00, 'Eye color' => 00, 'Ear outer color' => 00, 'Ear inner color' => 00, 'Frontpaws color' => 00, 'Backpaws color' => 00, 'Body color' => 00, 'Head pattern' => 00, 'Ear pattern' => 00, 'Frontpaws pattern' => 00, 'Backpaws pattern' => 00, 'Body pattern' => 00, 'Tail pattern' => 00];
 
 $nestedArray = [
-	'shapes' => [
+	'shapes' =>
+	[
 		'head' =>
 		[
 			'cat' => 0,
@@ -14,6 +15,34 @@ $nestedArray = [
 			'fox' => 0
 		],
 		'ear' =>
+		[
+			'cat' => 0,
+			'wolf' => 0,
+			'dog' => 20,
+			'fox' => 0
+		],
+		'mouth' =>
+		[
+			'cat' => 0,
+			'wolf' => 0,
+			'dog' => 20,
+			'fox' => 0
+		],
+		'frontpaws' =>
+		[
+			'cat' => 0,
+			'wolf' => 0,
+			'dog' => 20,
+			'fox' => 0
+		],
+		'backpaws' =>
+		[
+			'cat' => 0,
+			'wolf' => 0,
+			'dog' => 20,
+			'fox' => 0
+		],
+		'tail' =>
 		[
 			'cat' => 0,
 			'wolf' => 0,
@@ -21,34 +50,162 @@ $nestedArray = [
 			'fox' => 0
 		]
 	],
-	'colors' => [
+	'colors' =>
+	[
 		'head' =>
 		[
-			'red' => 5,
-			'blue' => 3,
-			'gray' => 9,
-			'brown' => 14,
-			'orange' => 2
+			'gray' => 5,
+			'lavender' => 3,
+			'khaki' => 9,
+			'darkgreen' => 14,
+			'chocolate' => 2,
+			'coral' => 2,
+			'lightpink' => 0,
+			'magenta' => 0,
+			'darksalmon' => 0,
+			'tomato' => 0,
+			'darkviolet' => 0,
+			'dodgerblue' => 0,
+			'hotpink' => 0,
+			'deeppink' => 0,
+			'goldenrod' => 0,
+			'cyan' => 0,
+			'darkslategray' => 0,
+			'red' => 0
 		],
 		'ear' =>
 		[
 			'outer' =>
 			[
-				'red' => 0,
-				'blue' => 0,
-				'gray' => 0,
-				'brown' => 0,
-				'orange' => 0
+				'gray' => 5,
+				'lavender' => 3,
+				'khaki' => 9,
+				'darkgreen' => 14,
+				'chocolate' => 2,
+				'coral' => 2,
+				'lightpink' => 0,
+				'magenta' => 0,
+				'darksalmon' => 0,
+				'tomato' => 0,
+				'darkviolet' => 0,
+				'dodgerblue' => 0,
+				'hotpink' => 0,
+				'deeppink' => 0,
+				'goldenrod' => 0,
+				'cyan' => 0,
+				'darkslategray' => 0,
+				'red' => 0
 			],
 			'inner' =>
 			[
-				'red' => 0,
-				'blue' => 0,
-				'gray' => 0,
-				'brown' => 0,
-				'orange' => 0
+				'gray' => 5,
+				'lavender' => 3,
+				'khaki' => 9,
+				'darkgreen' => 14,
+				'chocolate' => 2,
+				'coral' => 2,
+				'lightpink' => 0,
+				'magenta' => 0,
+				'darksalmon' => 0,
+				'tomato' => 0,
+				'darkviolet' => 0,
+				'dodgerblue' => 0,
+				'hotpink' => 0,
+				'deeppink' => 0,
+				'goldenrod' => 0,
+				'cyan' => 0,
+				'darkslategray' => 0,
+				'red' => 0
 			]
+		],
+		'eye' =>
+		[
+			'gray' => 5,
+			'lavender' => 3,
+			'khaki' => 9,
+			'darkgreen' => 14,
+			'chocolate' => 2,
+			'coral' => 2,
+			'lightpink' => 0,
+			'magenta' => 0,
+			'darksalmon' => 0,
+			'tomato' => 0,
+			'darkviolet' => 0,
+			'dodgerblue' => 0,
+			'hotpink' => 0,
+			'deeppink' => 0,
+			'goldenrod' => 0,
+			'cyan' => 0,
+			'darkslategray' => 0,
+			'red' => 0
+		],
+		'frontpaws' =>
+		[
+			'gray' => 5,
+			'lavender' => 3,
+			'khaki' => 9,
+			'darkgreen' => 14,
+			'chocolate' => 2,
+			'coral' => 2,
+			'lightpink' => 0,
+			'magenta' => 0,
+			'darksalmon' => 0,
+			'tomato' => 0,
+			'darkviolet' => 0,
+			'dodgerblue' => 0,
+			'hotpink' => 0,
+			'deeppink' => 0,
+			'goldenrod' => 0,
+			'cyan' => 0,
+			'darkslategray' => 0,
+			'red' => 0
+		],
+		'backpaws' =>
+		[
+			'gray' => 5,
+			'lavender' => 3,
+			'khaki' => 9,
+			'darkgreen' => 14,
+			'chocolate' => 2,
+			'coral' => 2,
+			'lightpink' => 0,
+			'magenta' => 0,
+			'darksalmon' => 0,
+			'tomato' => 0,
+			'darkviolet' => 0,
+			'dodgerblue' => 0,
+			'hotpink' => 0,
+			'deeppink' => 0,
+			'goldenrod' => 0,
+			'cyan' => 0,
+			'darkslategray' => 0,
+			'red' => 0
+		],
+		'body' =>
+		[
+			'gray' => 5,
+			'lavender' => 3,
+			'khaki' => 9,
+			'darkgreen' => 14,
+			'chocolate' => 2,
+			'coral' => 2,
+			'lightpink' => 0,
+			'magenta' => 0,
+			'darksalmon' => 0,
+			'tomato' => 0,
+			'darkviolet' => 0,
+			'dodgerblue' => 0,
+			'hotpink' => 0,
+			'deeppink' => 0,
+			'goldenrod' => 0,
+			'cyan' => 0,
+			'darkslategray' => 0,
+			'red' => 0
 		]
+	],
+	'patterns' =>
+	[
+
 	]
 ];
 
